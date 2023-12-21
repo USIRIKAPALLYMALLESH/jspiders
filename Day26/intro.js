864e58162acfe907dbe49aefed3a9b6feba236a3
mallesh <usirikapallymallesh@gmail.com>
2023-12-21 12:02:00 +0530

    the gradient colors and postion -relative class completed

---
+++ Day26/intro.js
@@ -0,0 +1,17 @@
+var a=10;
+var b=10.2;
+var c="mallesh";
+var d='c'
+var e=true;
+var f=BigInt(232);
+var g;
+var h=null;
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
+console.log(typeof(g));
+console.log(typeof(h));
+console.log(a+b);
